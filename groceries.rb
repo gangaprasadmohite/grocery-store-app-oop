9e7a0c2aef4367e0ab417d3eb227de9bb60b825a
shanky31 <gemohite@gmail.com>
2020-08-03 21:34:31 +0530

    Added groceries list

---
+++ groceries.rb
@@ -0,0 +1,10 @@
+module Groceries
+  items = [
+    {"name" => "milk", "price" => 3.97, "quantity" => 2, "discounted_price" => 5 },
+    {"name" => "bread", "price" => 2.17, "quantity" => 3, "discounted_price" => 6 },
+    {"name" => "banana", "price" => 0.99, "quantity" => nil, "discounted_price" => nil },
+    {"name" => "apple", "price" => 0.89, "quantity" => nil, "discounted_price" => nil }
+  ]
+  
+  item_names = ["milk", "bread", "banana", "apple"]
+end
